2ffeb9d161e23a9ee1adfb399ea84e349148a97a
Arturs <artos@osx.lv>
2018-12-05 20:04:53 +0200

    Uzlabots

--- latex_de.pl
+++ latex_de.pl
@@ -12,14 +12,16 @@ binmode(STDOUT, ":utf8");
 while (<IFH>) {
   #google hacks
   $_ =~ s/\ __\ /__/g;
+  $_ =~ s/__\ /__/g;
 
   $_ =~ s/__xx(.+?)xx__/\\qverb\|$1\|/g;
   $_ =~ s/__vv(.+?)vv__/\\cverb\|$1\|/g;
   $_ =~ s/__zz(.+?)zz__/\\begin\{$1\}/g;
   $_ =~ s/__yy(.+?)yy__/\\end\{$1\}/g;
+  $_ =~ s/__cc__/\\\\/g;
   $_ =~ s/__aa/\\/g;
-  $_ =~ s/__bb/\{/g;
-  $_ =~ s/bb__/\}/g;
+  $_ =~ s/bb__/\{/g;
+  $_ =~ s/__bb/\}/g;
   print;
 }
 
